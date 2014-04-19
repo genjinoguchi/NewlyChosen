d053226b9781812db2ede330a585eac354be7b0f
Genji Noguchi <genji.noguchi@gmail.com>
2014-04-19 17:10:30 -0400

    Added highlighting methods into ArrayChess, updated get and set methods into Piece.

--- ArrayChess.java
+++ ArrayChess.java
@@ -14,7 +14,7 @@ public class ArrayChess{
 		for(int y=0;y<Chess.numFiles;y++){
 			for(int x=0;x<Chess.numRanks;x++){
 				Chess.board[y][x]=new Square();
-				Chess.board[y][x].setPiece("Newly Chosen");
+				Chess.board[y][x].setPiece("_");
 			}
 		}
 	}
@@ -34,6 +34,49 @@ public class ArrayChess{
 		    oddLine = !oddLine;
 		}
     }
+
+    public void highlightCoveredSquares(String color){
+		//Establish highlight color	
+    	if(color.equals("Red")){
+	    	Color C = COLOR.Red;
+    	}else{
+	    	Color C = COLOR.Blue;
+	   	}
+
+		//for each in the board:
+		for(int y = 0; y < Chess.numRanks; y++){
+			for(int x = 0; x < Chess.numFiles; x++){
+
+				//check for each piece type and highlight appropriately:
+				if(!Chess.board[y][x].isEmpty() && Chess.board[y][x].getPieceColor().equals(color)){
+					if(Chess.board[y][x].getPieceOnSquare().equals("King")){
+						Chess.board[y][x].setColor(C);	
+						Chess.board[y][x].setColor(C);	
+						Chess.board[y][x].setColor(C);
+						Chess.board[y][x].setColor(C);
+						Chess.board[y][x].setColor(C);	
+						Chess.board[y][x].setColor(C);																
+					}
+					else if(Chess.board[y][x].getPieceOnSquare().equals("Queen")){
+ 
+					}
+					else if(Chess.board[y][x].getPieceOnSquare().equals("Rook")){
+
+					}
+					else if(Chess.board[y][x].getPieceOnSquare().equals("Bishop")){
+
+					}
+					else if(Chess.board[y][x].getPieceOnSquare().equals("Knight")){
+
+					}
+					else if(Chess.board[y][x].getPieceOnSquare().equals("Pawn")){
+
+					}
+
+				}
+			}
+		}
+	}
 
 	public String toString(){
 		String total = "";
